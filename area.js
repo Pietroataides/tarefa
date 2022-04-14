88865bca5e5a55f8e597eaa9eac4ed94ac0ed62c
theo <theos.@outlook.com>
2022-04-14 11:29:46 -0300

    1.0.0

---
+++ area.js
@@ -0,0 +1,10 @@
+const PI = Matemática. PI 
+Defina e exporte como funções circleArea() e squareArea() abaixo
+círculo de funçãoArea (raio) {
+		retorno PI * raio * raio
+}
+módulo. exportações. círculoArea = círculoArea;
+
+módulo. exportações. quadradoArea = função (lado) {
+		retorno lado * lado;
+}
---
+++ area2.js
@@ -0,0 +1,10 @@
+const { circleArea, squareArea } = require ('./shape-area.js');
+
+const raio = processo. argv[2];
+const lado = processo. argv[3];
+
+área ConstDeCircle = círculoArea (raio);
+área ConstOfSquare = quadradoArea (lado);
+
+console. log('A área do círculo ${raio} é ${areaRario.toFixed(2)}')
+console. log('A área do quadrado ${lado} é ${areaLado.toFixed(2)}')
